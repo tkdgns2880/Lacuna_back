b7e104f52246de574fa62893412d4b866caa7ddb
tkdgns2880 <tkdgns2880@naver.com>
2024-10-31 17:35:20 +0900

    order filter 추가

--- src/main/java/LacunaMatata/Lacuna/controller/admin/OrderManageController.java
+++ src/main/java/LacunaMatata/Lacuna/controller/admin/OrderManageController.java
@@ -2,10 +2,13 @@ package LacunaMatata.Lacuna.controller.admin;
 
 import LacunaMatata.Lacuna.dto.request.admin.order.ReqGetOrderListDto;
 import LacunaMatata.Lacuna.dto.response.admin.order.RespCountAndOderListDto;
+import LacunaMatata.Lacuna.dto.response.admin.order.RespGetOrderStatusFilterDto;
 import LacunaMatata.Lacuna.service.admin.OrderManageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /************************************************
  * version: 1.0.4                               *
@@ -23,50 +26,56 @@ public class OrderManageController {
 
     // 회원 주문 리스트 출력
     @GetMapping("/order/list")
-    ResponseEntity<?> getOrderList(@RequestBody ReqGetOrderListDto dto) {
+    public ResponseEntity<?> getOrderList(@RequestBody ReqGetOrderListDto dto) {
         RespCountAndOderListDto respCountAndOderListDto = orderManageService.getOrderList(dto);
         return ResponseEntity.ok().body(respCountAndOderListDto);
     }
 
     // 회원 주문 항목 상세 출력
     @GetMapping("/order/detail/{orderId}")
-    ResponseEntity<?> getOrderDetail() {
+    public ResponseEntity<?> getOrderDetail() {
         return ResponseEntity.ok().body(true);
     }
 
     // 회원 결제 항목 상세 출력
     @GetMapping("/pay/detail/{paymentId}")
-    ResponseEntity<?> getPaymentDetail() {
+    public ResponseEntity<?> getPaymentDetail() {
         return ResponseEntity.ok().body(true);
     }
 
     // 회원 결제 취소하기(결제 시스템)
     @PutMapping("/pay/system/cancel/{paymentId}")
-    ResponseEntity<?> cancelSystemPayment() {
+    public ResponseEntity<?> cancelSystemPayment() {
         return ResponseEntity.ok().body(true);
     }
 
     // 주문 수정 - 회원 결제 취소하기(계좌 이체)
     @PostMapping("/pay/account/cancel")
-    ResponseEntity<?> cancelAccountPayment() {
+    public ResponseEntity<?> cancelAccountPayment() {
         return ResponseEntity.ok().body(true);
     }
 
     // 주문 수정 - 회원 결제 승인하기(계좌 이체)
     @PostMapping("/pay/account/approve")
-    ResponseEntity<?> approveAccountPayment() {
+    public ResponseEntity<?> approveAccountPayment() {
         return ResponseEntity.ok().body(true);
     }
 
     // 회원 주문 항목 삭제
     @DeleteMapping("/pay/account/order/delete/{orderId}")
-    ResponseEntity<?> deleteOrder() {
+    public ResponseEntity<?> deleteOrder() {
         return ResponseEntity.ok().body(true);
     }
 
     // 회원 주문 항목 복수개 삭제
     @DeleteMapping("/pay/account/order/delete")
-    ResponseEntity<?> deleteOrderLIST() {
+    public ResponseEntity<?> deleteOrderLIST() {
         return ResponseEntity.ok().body(true);
+    }
+
+    @GetMapping("/status/filter")
+    public ResponseEntity<?> getOrderStatusFilter() {
+        List<RespGetOrderStatusFilterDto> orderFilter = orderManageService.getOrderStatusFilter();
+        return ResponseEntity.ok().body(orderFilter);
     }
 }
---
+++ src/main/java/LacunaMatata/Lacuna/dto/response/admin/order/RespGetOrderStatusFilterDto.java
@@ -0,0 +1,11 @@
+package LacunaMatata.Lacuna.dto.response.admin.order;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class RespGetOrderStatusFilterDto {
+    private int orderId;
+    private String status;
+}
