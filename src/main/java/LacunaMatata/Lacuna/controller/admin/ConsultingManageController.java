a28e26879c2273e5a4bf5789ebdf35144f68294c
ryeong99 <lovolvo1015@gmail.com>
2024-10-31 16:27:17 +0900

    컨설팅 페이지

--- src/main/java/LacunaMatata/Lacuna/controller/admin/ConsultingManageController.java
+++ src/main/java/LacunaMatata/Lacuna/controller/admin/ConsultingManageController.java
@@ -1,4 +1,177 @@
 package LacunaMatata.Lacuna.controller.admin;
 
+import LacunaMatata.Lacuna.service.admin.ConsultingManageService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/************************************************
+ * version: 1.0.4                               *
+ * author: 정령우                                *
+ * description: AdminMbtiController() - 세팅     *
+ * createDate: 2024-10-30                       *
+ * updateDate: 2024-10-30                       *
+ ************************************************/
+
+@RestController
+@RequestMapping("/api/v1/admin/consulting")
+@Api(tags = {"consultingManageController"})
 public class ConsultingManageController {
+
+    @Autowired
+    private ConsultingManageService consultingManageService;
+
+    // 컨설팅 상위 분류 목록 출력
+    @GetMapping("/upper/list")
+    ResponseEntity<?> getUpperConsultingListApi() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 상위 분류 항목 출력(필터)
+    @GetMapping("/upper/filter")
+    ResponseEntity<?> getUpperConsultingListFilter() {return ResponseEntity.ok().body(null); }
+
+    // 컨설팅 상위 분류 항목 등록
+    @PostMapping("/upper/regist")
+    ResponseEntity<?> registUpperConsulting() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 상위 분류 항목 출력
+    @GetMapping("/upper/{upperId}")
+    ResponseEntity<?> getUpperConsulting() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 상위 분류 항목 수정
+    @PutMapping("/upper/modify/{upperId}")
+    ResponseEntity<?> modifyUpperConsulting() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 상위 분류 항목 삭제
+    @DeleteMapping("/upper/delete/{upperId}")
+    ResponseEntity<?> deleteUpperConsulting() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 상위 분류 항목 복수개 삭제
+    @DeleteMapping("/upper/delete")
+    ResponseEntity<?> deleteUpperConsultingList() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 하위 분류 목록 출력
+    @GetMapping("/lower/list")
+    ResponseEntity<?> getLowerConsultingListApi() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 하위 분류 항목 출력 (필터)
+    @GetMapping("/lower/list/filter/{upperId}")
+    ResponseEntity<?> getLowerConsultingListFilter() {return ResponseEntity.ok().body(null); }
+
+    // 컨설팅 하위 분류 항목 등록
+    @PostMapping("/lower/regist")
+    ResponseEntity<?> registLowerConsulting() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 하위 분류 항목 출력
+    @GetMapping("/lower/{upperId}")
+    ResponseEntity<?> getLowerConsulting() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 하위 분류 항목 수정
+    @PutMapping("/lower/modify/{upperId}")
+    ResponseEntity<?> modifyLowerConsulting() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 하위 분류 항목 삭제
+    @DeleteMapping("/lower/delete/{upperId}")
+    ResponseEntity<?> deleteLowerConsulting() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 하위 분류 항목 복수개 삭제
+    @DeleteMapping("/lower/delete")
+    ResponseEntity<?> deleteLowerConsultingList() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 설문지 목록 출력
+    @GetMapping("/servey/list")
+    ResponseEntity<?> getServeyConsultingListApi() {
+        return ResponseEntity.ok().body(null);
+    }
+
+
+    // 컨설팅 설문지 항목 등록
+    @PostMapping("servey/regist")
+    ResponseEntity<?> registServeyConsulting() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 설문지 항목 출력
+    @GetMapping("/servey/{consultingId}")
+    ResponseEntity<?> getServeyConsulting() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 설문지 항목 수정
+    @PutMapping("/servey/modify")
+    ResponseEntity<?> modifyServeyConsulting() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 설문지 항목 삭제
+    @DeleteMapping("/servey/delete/{consultingId}")
+    ResponseEntity<?> deleteServeyConsulting() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 설문지 항목 복수개 삭제
+    @DeleteMapping("/servey/delete")
+    ResponseEntity<?> deleteServeyConsultingList() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 결과지 목록 출력
+    @GetMapping("/servey/result/list")
+    ResponseEntity<?> getResultConsultingListApi() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 결과지 항목 등록
+    @PostMapping("/servey/result/regist")
+    ResponseEntity<?> registResultConsulting() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 결과지 항목 출력
+    @GetMapping("/servey/result/{resultId}")
+    ResponseEntity<?> getResultConsulting() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 결과지 항목 수정
+    @PutMapping("/servey/result/modify/{resultId}")
+    ResponseEntity<?> modifyResultConsulting() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 결과지 항목 삭제
+    @DeleteMapping("/servey/result/delete/{resultId}")
+    ResponseEntity<?> deleteResultConsulting() {
+        return ResponseEntity.ok().body(null);
+    }
+
+    // 컨설팅 결과지 항목 복수개 삭제
+    @DeleteMapping("/servey/result/delete")
+    ResponseEntity<?> deleteResultConsultingList() {
+        return ResponseEntity.ok().body(null);
+    }
 }
--- src/main/java/LacunaMatata/Lacuna/service/admin/ConsultingManageService.java
+++ src/main/java/LacunaMatata/Lacuna/service/admin/ConsultingManageService.java
@@ -1,8 +1,152 @@
 package LacunaMatata.Lacuna.service.admin;
 
+import LacunaMatata.Lacuna.repository.admin.ConsulttingManageMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Service
 public class ConsultingManageService {
 
+    @Autowired
+    private ConsulttingManageMapper consultingManageMapper;
+
+    // 컨설팅 상위 분류 목록 출력
+    public void getUpperConsultingList() {
+
+    }
+
+    // 컨설팅 상위 분류 항목 출력(필터)
+    public void getUpperConsultingFilter() {
+
+    }
+
+    // 컨설팅 상위 분류 항목 등록
+    public void registUpperConsulting() {
+
+    }
+
+    // 컨설팅 상위 분류 항목 출력
+    public void getUpperConsulting() {
+
+    }
+
+    // 컨설팅 상위 분류 항목 수정
+    public void modifyUpperConsulting() {
+
+    }
+
+    // 컨설팅 상위 분류 항목 삭제
+    public void deleteUpperConsulting() {
+
+    }
+
+    // 컨설팅 상위 분류 항목 복수개 삭제
+    public void deleteUpperConsultingList() {
+
+    }
+
+    // 컨설팅 하위 분류 목록 출력
+    public void getLowerConsultingList() {
+
+    }
+
+    // 컨설팅 하위 분류 항목 출력 (필터)
+    public void getLowerConsultingFilter() {
+
+    }
+
+    // 컨설팅 하위 분류 항목 등록
+    public void registLowerConsulting() {
+
+    }
+
+    // 컨설팅 하위 분류 항목 출력
+    public void getLowerConsulting() {
+
+    }
+
+    // 컨설팅 하위 분류 항목 수정
+    public void modifyLowerConsulting() {
+
+    }
+
+    // 컨설팅 하위 분류 항목 삭제
+    public void deleteLowerConsulting() {
+
+    }
+
+    // 컨설팅 하위 분류 항목 복수개 삭제
+    public void deleteLowerConsultingList() {
+
+    }
+
+    // 컨설팅 설문지 목록 출력
+    public void getServeyList() {
+
+    }
+
+    // 컨설팅 설문지 항목 출력(필터)
+    public void getServeyFilter() {
+
+    }
+
+    // 컨설팅 설문지 항목 등록
+    public void registServey() {
+
+    }
+
+    // 컨설팅 설문지 항목 출력
+    public void getServey() {
+
+    }
+
+    // 컨설팅 설문지 항목 수정
+    public void modifyServey() {
+
+    }
+
+    // 컨설팅 설문지 항목 삭제
+    public void deleteServey() {
+
+    }
+
+    // 컨설팅 설문지 항목 복수개 삭제
+    public void deleteServeyList() {
+
+    }
+
+    // 컨설팅 결과지 목록 출력
+    public void getResultList() {
+
+    }
+
+    // 컨설팅 결과지 항목 등록
+    public void registResult() {
+
+    }
+
+    // 컨설팅 결과지 항목 출력
+    public void getResult() {
+
+    }
+
+    // 컨설팅 결과지 항목 수정
+    public void modifyResult() {
+
+    }
+
+    // 컨설팅 결과지 항목 삭제
+    public void deleteResult() {
+
+    }
+
+    // 컨설팅 결과지 항목 복수개 삭제
+    public void deleteResultList() {
+
+    }
 }
